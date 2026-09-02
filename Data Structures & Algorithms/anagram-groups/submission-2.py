0fca2c48dae66c147387b43121af48ae8d0f599e
Keane Manuel <keanemanuel1@gmail.com>
2026-09-02 21:47:53 +1000

    Bulk sync: 33 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = []
+        hash = {}
+        for word in strs:
+            key = str(sorted(word))
+            if key in hash:
+                hash[key].append(word)
+            else:
+                hash[key] = [word]
+        
+        for key in hash:
+            groups.append(hash[key])
+        
+        return groups
+
+
+            
---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash = defaultdict(list)
+
+        for s in strs:
+            key = [0]*26
+            for c in s:
+                key[ord(c) - ord('a')] += 1
+            
+            hash[tuple(key)].append(s)
+        return list(hash.values())
+            
+        
